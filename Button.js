0263ca457fc668ba5c6c32fb87e82e306ba930b4
Yorn <yornclaudel@gmail.com>
2019-03-11 19:02:11 +0100

    basic button label handling

--- Button.js
+++ Button.js
@@ -16,6 +16,8 @@ class Button extends Drawable {
     draw() {
         this.ctx.fillStyle = "#FF0000";
         this.ctx.fillRect(this.dx, this.dy, this.width, this.height);
+        this.ctx.font = "15px Arial";
+        this.ctx.strokeText(this.label, this.x-this.width/4, this.y+this.height/4); 
     }
 
 }
