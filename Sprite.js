c1b218619ff06f951f04707bf2f8dc7664e67f77
Yorn <yornclaudel@gmail.com>
2019-03-12 19:16:36 +0100

    working on basic sprite system

---
+++ Sprite.js
@@ -0,0 +1,13 @@
+class Sprite extends Drawable{
+
+    constructor(x, y, width, height,ctx,img){
+        super(x, y, width, height,ctx);
+        this.img=img;
+    }
+
+    draw(){
+        this.ctx.drawImage();
+        
+    }
+
+}
