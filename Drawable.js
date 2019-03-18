f0c5dd0a9416ee6a6c7716251072e62768fe4945
Thomas_Bosch <thomasbosch36@gmail.com>
2019-03-18 09:43:36 +0100

    started pickup

--- Drawable.js
+++ Drawable.js
@@ -1,14 +1,10 @@
 class Drawable {
 
-    constructor(x, y, width, height,ctx) {
+    constructor(width, height,ctx) {
         if (this.constructor == Drawable)
             throw new TypeError("Cannot instantiate Drawable Type, extend it instead");
-        this.x = x;
-        this.y = y;
         this.height=height;
         this.width=width;
-        this.dx=this.x-width/2;
-        this.dy=this.y-height/2;
         this.ctx=ctx;
     }
 
--- Pickup.js
+++ Pickup.js
@@ -1,7 +1,14 @@
 class Pickup{
 
-    constructor(){
+    constructor(x,y, sprite){
+    	this.x=5;
+    	this.y=5;
+    	this.Sprite=sprite;
         
+    }
+
+    this.sprite.draw(this.x, this.y);
+
     }
 
 }
